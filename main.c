5ffccdd0bce1236637f9db37faab67b569d6c0cb
Constantin Dullo <constantindullo@cdullo.ftp.sh>
2018-12-26 20:19:00 +0100

    adding all other relevant files

---
+++ main.c
@@ -0,0 +1,29 @@
+//___FILEHEADER___
+
+#include "___FILEBASENAME___.h"
+
+int counter = 0;
+int offset = 5;
+
+void calc(void) {
+	i += off;
+}
+
+int main(void) {
+    // setup perifery hear
+
+    // main loop
+	while (1) {
+        calc();
+        // insert code hear
+	}
+	return 0; // never reached
+}
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t* file, uint32_t line) {
+    /* Infinite loop */
+    /* Use GDB to find out why we're here */
+    while (1);
+}
+#endif
