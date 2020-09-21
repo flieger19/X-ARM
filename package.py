b59c39bd36e1cb0e8aed75686c76f414d876eb04
Constantin Dullo <constantin.dullo@me.com>
2020-09-21 17:42:52 +0200

    Adding function install files to the package source file

--- package.py
+++ package.py
@@ -3,3 +3,13 @@ Documentation, License etc.
 
 @package X-ARM Xcode template for embedded arm cortex-m development
 """
+
+import shutil
+
+
+def install_files(destination, files):
+    """
+    Install files in a given directory
+    :param destination: Directory to install files to
+    :param files: Files to install in a given directory
+    """
