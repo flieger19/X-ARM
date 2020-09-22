359295c85a9640df581de203b8541dcec9aed1fe
Constantin Dullo <constantin.dullo@me.com>
2020-09-22 14:52:45 +0200

    Implementing directory iterator function of the package source file

--- package.py
+++ package.py
@@ -57,6 +57,30 @@ def directory_iterator(source, target):
     :param source: Directory to iterate through
     :param target: Directory to symlink files to
     """
+    for file in os.listdir(source):
+        filename = os.fsdecode(file)
+        path_source = source + bytes("/", 'utf-8') + file
+        path_target = target + bytes("/", 'utf-8') + file
+        if os.path.isdir(path_source):
+            try:
+                os.makedirs(os.fsdecode(path_target))
+            except OSError:
+                print("Creation of the directory %s failed" % os.fsdecode(path_target))
+            directory_iterator(os.fsencode(path_source), os.fsencode(path_target))
+        elif os.path.isfile(path_source):
+            try:
+                os.symlink(path_source, path_target)
+            except:
+                print("Symlink Error")
+        elif os.access(path_source, os.X_OK):
+            try:
+                os.symlink(path_source, path_target)
+            except:
+                print("Symlink Error")
+        elif os.path.islink(path_source):
+            continue
+        else:
+            print("Special file ", path_source)
 
 
 def install_sdk_files(destination_directory):
